23472b1e70c3aef84e972f983c0e63421d389f8f
cuto <127745126+giuliagagliardini@users.noreply.github.com>
2023-03-13 15:50:15 +0100

    Add files via upload

---
+++ #include <stdio.h>.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int numero = 17;
+    double numerolungo = 574384773;
+    float numeroconlavirgola = 5.346;
+    char carattere = 'c';
+
+    printf("%d\n", numero);
+printf("numero lungo è: %f, numero con la virgola è: %f\n", numerolungo, numeroconlavirgola);
+printf("il carattere è %c\n"", carattere)
+}
---
+++ esempio.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("ciao il mio nome è giulia\n");
+}
