12894d6bc89920f5425d09bfc8c7b4fabad6e267
cuto <127745126+giuliagagliardini@users.noreply.github.com>
2023-03-13 16:50:43 +0100

    Add files via upload

---
+++ ciaooo.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main ()
+{
+    int x;
+int y;
+
+    printf("ïnserisci un numero: ");
+scanf("%d" , &x);
+  printf("ïnserisci un altro numero: ");
+  scanf("%d" , &y);
+printf("Ï numeri inseriti sono x = %d e y = %d\n", x, y);
+}
+   
