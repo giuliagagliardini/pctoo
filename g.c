2500eb29afaf55aa5174a19ff8f1072b767116f4
cuto <127745126+giuliagagliardini@users.noreply.github.com>
2023-03-14 11:14:13 +0100

    Add files via upload

---
+++ g.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+int eta;
+
+
+printf("quanti anni hai\n");
+scanf("%d", &eta);
+if (eta < 18)
+{
+    printf("la condizione è verificata\n");
+}
+else
+{
+    printf("la condizione non è verificata\n");
+}
+
+}
