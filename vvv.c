84d22d601c6b2e51a749c0c58f5abd9cd34dc5be
cuto <127745126+giuliagagliardini@users.noreply.github.com>
2023-03-16 14:26:25 +0100

    Add files via upload

---
+++ vvv.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    char stringa[] =  "";
+
+    printf("ütente inserisci una sringa: ");
+    scanf(" %s", stringa);
+
+    int i = 0;
+    while (stringa{i} != '\0')
+    {
+        i++;
+    }
+
+    while (i >= 0)
+    {
+        printf("%c", stringa[i]);
+        i--;
+    }
+}
